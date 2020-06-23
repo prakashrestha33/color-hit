e41e226fe77e1526d0048035615a7355438d0cf9
Prakash Shrestha <sthaprakash91@gmail.com>
2020-06-23 16:35:53 +0545

    Updates

---
+++ precache-manifest.49e5457bf994eaedf1ffc2aae0c6b690.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8de621642863f3a3f1cfde57d6e711e",
+    "url": "/color-hit/index.html"
+  },
+  {
+    "revision": "9368b951e038b405f448",
+    "url": "/color-hit/static/css/main.fe378451.chunk.css"
+  },
+  {
+    "revision": "c65c797488ae16a60b9e",
+    "url": "/color-hit/static/js/2.a7c3ac6b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/color-hit/static/js/2.a7c3ac6b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9368b951e038b405f448",
+    "url": "/color-hit/static/js/main.9c95fc3f.chunk.js"
+  },
+  {
+    "revision": "fcc831d1976cb439dc55",
+    "url": "/color-hit/static/js/runtime-main.81a0836c.js"
+  },
+  {
+    "revision": "3c70fa8f73d054324d10a6c8d712980e",
+    "url": "/color-hit/static/media/rain.3c70fa8f.svg"
+  }
+]);
